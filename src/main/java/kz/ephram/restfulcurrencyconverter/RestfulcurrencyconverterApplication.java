19a5e7f0281c6af58488d0c90d0e4518448c2c81
Ephram <ibraev.dulat@gmail.com>
2023-11-16 07:53:33 +0600

    Start project

---
+++ src/main/java/kz/ephram/restfulcurrencyconverter/RestfulcurrencyconverterApplication.java
@@ -0,0 +1,13 @@
+package kz.ephram.restfulcurrencyconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulcurrencyconverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulcurrencyconverterApplication.class, args);
+	}
+
+}
